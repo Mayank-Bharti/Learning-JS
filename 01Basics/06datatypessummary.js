203ee72d62b7455f038b5c94697f35284fed2901
Mayank-Bharti <128963496+Mayank-Bharti@users.noreply.github.com>
2024-03-08 14:05:12 +0000

    datatypes_summary

--- 01Basics/06datatypessummary.js
+++ 01Basics/06datatypessummary.js
@@ -35,3 +35,22 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// memory: 1) stack (primitive ): copy milta h
+//2) Heap(non-primitive): reference milta h
+
+let myname='mayank'
+let anothername=myname
+anothername='madhuri'
+console.log(myname)
+console.log(anothername)
+let userone = {
+    email:"123@gmail.com",
+    upi:"123@ybl"
+}
+let usertwo=userone
+usertwo.email="456@gmail.com"
+console.log(usertwo.email)
+console.log(userone.email)
