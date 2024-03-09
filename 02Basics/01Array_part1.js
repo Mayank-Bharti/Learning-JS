7e0d0d3a05896db6876ccb05f464e2a95e800979
Mayank-Bharti <128963496+Mayank-Bharti@users.noreply.github.com>
2024-03-09 11:14:15 +0000

    Array_part1

--- 02Basics/01Array_part1.js
+++ 02Basics/01Array_part1.js
@@ -2,43 +2,43 @@
 // array
 //when we initialize arr in console browser then we get again protype and properties of arr
 const Number=[0,1,2,3,4,5]
-console.log(Number)
+console.log(Number) //[0,1,2,3,4,5]
 
 const myArr = [0, 1, 2, 3, 4, 5]
 const myHeors = ["shaktiman", "naagraj"]
 
-const myArr2 = new Array(1, 2, 3, 4)
-console.log(myArr2);
-console.log(myArr[1]);
+const myArr2 = new Array(1, 2, 3, 4) 
+console.log(myArr2); //[1,2,3,4]
+console.log(myArr[1]);//1
 
 // Array methods
 
  myArr.push(6) //6 is push at last of array
- console.log(myArr)
- myArr.push(7)
- console.log(myArr)
+ console.log(myArr) //[0,1,2,3,4,5,6]
+ myArr.push(7) 
+ console.log(myArr) //[0,1,2,3,4,5,6,7]
  myArr.pop()
- console.log(myArr)//last elements of array get removed
+ console.log(myArr)//last elements of array get removed [0,1,2,3,4,5,6]
 
  myArr.unshift(9) //we have to shift every value of array and 9 is place in first position
                   //so for large value its take much time and space.So unshift is very bad for programmers
                   //But for any work which i need data to appear in first in array,we have to use it because before 
                   //optmisation ,work is priority.
- console.log(myArr)
+ console.log(myArr) //[9,0,1,2,3,4,5,6]
  myArr.shift() //it removes first elements of array
- console.log(myArr)
+ console.log(myArr)//[0,1,2,3,4,5,6]
 
- console.log(myArr.includes(9));
- console.log(myArr.includes(3));
- console.log(myArr.indexOf(3));
- console.log(myArr.indexOf(9));
+ console.log(myArr.includes(9));//false
+ console.log(myArr.includes(3));//true
+ console.log(myArr.indexOf(3));//3
+ console.log(myArr.indexOf(9));//-1
  
  const newArr = myArr.join()
 
- console.log(myArr);
- console.log( newArr);
- console.log(typeof(myArr));
- console.log(typeof(newArr));
+ console.log(myArr);//[0,1,2,3,4,5,6]
+ console.log( newArr);//0,1,2,3,4,5,6
+ console.log(typeof(myArr));//object
+ console.log(typeof(newArr));//string
 
 
 // slice, splice
@@ -51,12 +51,12 @@ console.log(myArr[1]);
 //One more visibility difference is , this is not main difference , in slice ending range value is not included 
 //and in splice it also included.
 
-console.log("A ", myArr);
+console.log("A ", myArr);//A [0,1,2,3,4,5,6]
 const myn1 = myArr.slice(1, 3)
 console.log(myn1);//[1,2]
-console.log("B ", myArr);//[0,1,2,3,4,5,6]
+console.log("B ", myArr);//B [0,1,2,3,4,5,6]
 
 
 const myn2 = myArr.splice(1, 3)
 console.log(myn2);    // [1,2,3]
-console.log("C ", myArr); //[0,4,5,6]
+console.log("C ", myArr); //C [0,4,5,6]
