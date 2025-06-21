e72d3f2f4c8654f75e433a94864043274a1352bf
Mayank-Bharti <mayankbharti349@gmail.com>
2025-06-21 11:27:50 +0530

    commit

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -1,3 +1,6 @@
+//for iterate object
+
+// for in ==> loop is used for object
 
 const myObject = {
     js: 'javascript',
@@ -5,16 +8,27 @@ const myObject = {
     rb: "ruby",
     swift: "swift by apple"
 }
+//
 
 for (const key in myObject) {
-    //console.log(`${key} shortcut is for ${myObject[key]}`);
+    console.log(key);
+}
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
 }
 
+//for in ==> used in array
 const programming = ["js", "rb", "py", "java", "cpp"]
 
 for (const key in programming) {
-    //console.log(programming[key]);
+    console.log(key);
 }
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
+
+//for in ==> used in map... it is not iterable
 
 // const map = new Map()
 // map.set('IN', "India")
--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -1,5 +1,5 @@
 
-// for of
+// for of ==> array and map specific loop
 
 // ["", "", ""]
 // [{}, {}, {}]
@@ -7,12 +7,12 @@
 const arr = [1, 2, 3, 4, 5]
 
 for (const num of arr) {
-    //console.log(num);
+    console.log(num);
 }
 
 const greetings = "Hello world!"
 for (const greet of greetings) {
-    //console.log(`Each char is ${greet}`)
+    console.log(`Each char is ${greet}`)
 }
 
 // Maps
@@ -24,16 +24,28 @@ map.set('Fr', "France")
 map.set('IN', "India")
 
 
-// console.log(map);
+ console.log(map);
+
+ for (const key of map) {
+    console.log(key);
+}
+
+//destructure of array
 
 for (const [key, value] of map) {
-    // console.log(key, ':-', value);
+    
+    
+     console.log(key, ':-', value);
 }
+
 
 const myObject = {
     game1: 'NFS',
     game2: 'Spiderman'
 }
+
+//this is not iterable...for iterate object there is another method...
+//this method is for map
 
 // for (const [key, value] of myObject) {
 //     console.log(key, ':-', value);
